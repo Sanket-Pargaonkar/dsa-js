31fcb3654928330266f985e17011907e15751944
sanket.pargaonkar <sanket.pargaonkar@gmail.com>
2024-09-24 06:36:22 +0530

    adds solution for gl_course/array-max-diff.js problem

--- problems/demo.js
+++ problems/demo.js
@@ -1,11 +1,16 @@
  const { missing_num   } = require('./gl_course/missing-number'); // 
 const { largest_lr   } = require('./gl_course/largest-left-right');
 const {reverse_array } = require('./gl_course/reverse-array')
+const { array_max_diff } = require('./gl_course/array-max-diff')
+
 // console.log(missing_num([1,2,4,5]));
 // console.log(missing_num([1,2,4,5],missing_num.solutions.optimisedSolution ));
 largest_lr([4,2,1,3,6,4])
 largest_lr([4,2,1,3,6,4],largest_lr.solutions.optimisedSolution)
 // reverse_array([4,2,1,3,6,4])
 // reverse_array([4,2,1,3,6,4], reverse_array.twoPointerSolution)
-reverse_array([4,2,1,3,6])
-reverse_array([4,2,1,3,6], reverse_array.twoPointerSolution)
+// reverse_array([4,2,1,3,6])
+// reverse_array([4,2,1,3,6], reverse_array.twoPointerSolution)
+// console.log(reverse_array.problemStatement)
+console.log(array_max_diff.solutions.problemStatement)
+array_max_diff([7, 1, 5, 3, 6, 4])
---
+++ problems/gl_course/array-max-diff.js
@@ -0,0 +1,26 @@
+const { createPrototypeSolution } = require('../utils/solutionsFactory');
+const exportSolution = require('../utils/export-uitil')
+
+const sol = createPrototypeSolution();
+sol.problemStatement = `Given prices of stocks price[i] on i th day, could you compute the max profit that could be computed by
+doing only 1 transaction.
+
+Example
+Input: prices!] = (7, 1, 5, 3, 6, 4]
+Output: 5`
+
+sol.defaultSolution = (data) => {
+	console.log('Max diff ', data, defSol(data))
+}
+function defSol(data) {
+    let md = data[0] - data[1]
+	for (let i = 2; i < data.length; i++) {
+		if (data[0] - data[i] > md) {
+			md = data[0] - data[i]
+		}
+	}
+	return md;
+}
+
+
+module.exports = { array_max_diff: exportSolution(sol) }
