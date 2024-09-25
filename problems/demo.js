bef0f4b2c445fc9012e5c86ffa94580ecae46f7d
sanket.pargaonkar <sanket.pargaonkar@gmail.com>
2024-09-25 12:27:40 +0530

    adds solution to new problem /leetcode/isArrSorted.js

--- problems/demo.js
+++ problems/demo.js
@@ -4,6 +4,8 @@ const {reverse_array } = require('./gl_course/reverse-array')
 const { array_max_diff } = require('./gl_course/array-max-diff')
 const { largest_n  } = require('./geeksforgeeks/largest-n')
 const {secondL} = require('./geeksforgeeks/second-largest')
+const { isArrSorted} = require('./leetcode/isArrSorted')
+
 // console.log(missing_num([1,2,4,5]));
 // console.log(missing_num([1,2,4,5],missing_num.solutions.optimisedSolution ));
 largest_lr([4,2,1,3,6,4])
@@ -18,4 +20,8 @@ array_max_diff([7, 1, 5, 3, 6, 4])
 largest_n([7, 1, 5, 3, 6, 4])
 
 console.log('SecondLargest : ',[7, 1, 5, 3, 6, 4] ,secondL([7, 1, 5, 3, 6, 4]))
+console.log('isArrSorted  : ',[7, 1, 5, 3, 6, 4] ,isArrSorted([7, 1, 5, 3, 6, 4]))
+console.log('isArrSorted  : ',[7, 5, 4] ,isArrSorted([7, 5, 4]))
+console.log('isArrSorted  : ',[7, 5, 5, 4] ,isArrSorted([7, 5, 5, 4]))
+console.log('isArrSorted  : ',[7, 5, 5, 6] ,isArrSorted([7, 5, 5, 6]))
 
---
+++ problems/leetcode/isArrSorted.js
@@ -0,0 +1,63 @@
+const { createPrototypeSolution } = require('../utils/solutionsFactory');
+const exp = require('../utils/export-uitil')
+
+const sols = createPrototypeSolution();
+
+sols.problemStatement = `Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+ `
+sols.ulr = 'https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/'
+sols.defaultSolution = function (data) {
+	// all equal, all desc, all asc, mix of equal and asc/desc
+	//check if asc/dessc
+	let asc = false, desc = false;
+	// [ 7, 1, 5, 3, 6, 4 ]
+	for (let i = 0; i < data.length - 1; i++) {
+		if (data[i] <= data[i + 1]) {
+			asc = true
+		} else {
+			asc = false
+			break
+		}
+
+	}
+	if (asc) return asc
+
+	for (let i = 0; i < data.length - 1; i++) {
+		if (data[i] >= data[i + 1]) {
+			desc = true
+		} else {
+			desc = false
+			break
+		}
+
+	}
+	return desc
+
+
+}
+
+module.exports = { isArrSorted: exp(sols) }
